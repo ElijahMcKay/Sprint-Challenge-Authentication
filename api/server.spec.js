bd93f8c3318eba409849910f570566cd5fb7a410
ElijahMcKay <45011056+ElijahMcKay@users.noreply.github.com>
2019-09-20 11:31:13 -0500

    writing tests

---
+++ api/server.spec.js
@@ -0,0 +1,33 @@
+//importing server and supertest
+const request = require('supertest'); 
+const server = require('./server.js'); 
+
+
+describe('server.js', () => {
+
+    const creds = {
+        username: "tim",
+        password: "tim"
+    }
+
+    const newCreds = {
+        username: 'gracie', 
+        password: 'gracie',
+    }
+
+    describe(' login route ', () => {
+        it('should return a status code of 201', () => { 
+            return request(server).post('/api/auth/login')
+                .send(creds)
+                .expect(201)
+        })
+    })
+
+    describe('register route', () => {
+        it('it should return a status of 201', () => {
+            return request(server).post('/api/auth/register')
+                .send(newCreds)
+                .expect(201) 
+        })
+    })
+})
